91c1bf8652f2d22bb872490bf0d32be3c6c7dd87
Mtsumi <mtsumijr@gmail.com>
2022-03-12 02:15:21 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,30 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Aly Mtsumi
+ */
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int d;
+	int digit1, digit2;
 
-for (d = 0; d < 90; d++)
-{
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 
-putchar((d / 10) + '0');
-putchar((d % 10) + '0');
+		}
 
-if (d != 89)
-{
-putchar(',');
-putchar(' ');
-}
-}
 putchar('\n');
 return (0);
 }
