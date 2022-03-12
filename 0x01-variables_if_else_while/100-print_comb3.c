bdad6945e8e0fe4904edc06adf7c453021803a01
Mtsumi <mtsumijr@gmail.com>
2022-03-11 18:22:32 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -10,23 +9,20 @@
 
 int main(void)
 {
-
 int d;
 
 for (d = 0; d < 90; d++)
-
 {
 
-for ()
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
+
 if (d != 89)
+{
 putchar(',');
 putchar(' ');
 }
-
+}
 putchar('\n');
-
 return (0);
-
 }
