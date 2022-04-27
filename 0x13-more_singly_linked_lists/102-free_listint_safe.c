cdffc47d93e58bfdb537037b7a39c3f727b7510a
Mtsumi <mtsumijr@gmail.com>
2022-04-27 18:14:56 +0000

    task 12 and 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -33,12 +33,13 @@ size_t looped_listint_count(listint_t *head)
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
+			tortoise = tortoise->next;
 
-																						tortoise = tortoise->next;
-																						while (tortoise != hare)	
-				{									nodes++;
-				tortoise = tortoise->next;
-				}									return (nodes);
+		while (tortoise != hare)
+		{
+			nodes++;
+			tortoise = tortoise->next;
+		}									return (nodes);
 		}
 
 		tortoise = tortoise->next;
